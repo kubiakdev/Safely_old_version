46c7a63b3062241a59324bb4dd4b73ba5f792061
kubiakpatryk <kubiakpatryk@onet.eu>
2017-10-17 11:01:53 +0200

    Add SharedPreferencesManager and first tests using Roboletric library.

--- app/src/main/java/com/kubiakpatryk/safely/PreferenceOperations.java
+++ app/src/main/java/com/kubiakpatryk/safely/PreferenceOperations.java
@@ -15,18 +15,7 @@
  */
 package com.kubiakpatryk.safely;
 
-import static org.junit.Assert.*;
-
-import org.junit.Test;
-
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
-public class ExampleUnitTest {
-    @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
-    }
+public interface PreferenceOperations {
+    Object getIfContainsKey(String key);
+    void setKeyValue(String key, Object value);
 }
