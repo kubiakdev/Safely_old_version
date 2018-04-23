24875b91e94d96953506aa4e89fc606322c5d90e
kubiakpatryk <kubiakdeveloper@gmail.com>
2018-04-23 08:17:27 +0200

    Add autofocus in NoteDialog and delete license statements in each file.

--- app/src/main/java/com/kubiakpatryk/safely/ui/main/dialog/NoteDialogFragment.java
+++ app/src/main/java/com/kubiakpatryk/safely/ui/main/dialog/NoteDialogFragment.java
@@ -1,18 +1,3 @@
-/*
- * Copyright (C) 2018 Patryk Kubiak
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.kubiakpatryk.safely.ui.main.dialog;
 
 import android.os.Bundle;
@@ -22,6 +7,7 @@ import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.EditText;
 
 import com.kubiakpatryk.safely.R;
@@ -44,7 +30,7 @@ public class NoteDialogFragment extends BaseDialogFragment implements NoteDialog
     @BindView(R.id.note_layout_editText_noteContent)
     EditText editText;
 
-    public static NoteDialogFragment newInstance(String content){
+    public static NoteDialogFragment newInstance(String content) {
         NoteDialogFragment dialog = new NoteDialogFragment();
         Bundle bundle = new Bundle();
         bundle.putString("current_content", content);
@@ -59,7 +45,7 @@ public class NoteDialogFragment extends BaseDialogFragment implements NoteDialog
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.note_layout, container, false);
 
-        if (getActivityComponent() != null){
+        if (getActivityComponent() != null) {
             getActivityComponent().inject(this);
             setUnbinder(ButterKnife.bind(this, view));
             presenter.onAttach(this);
@@ -70,10 +56,18 @@ public class NoteDialogFragment extends BaseDialogFragment implements NoteDialog
                 editText.setText(content);
             }
         }
+
         return view;
     }
 
-    public void show(FragmentManager fragmentManager){
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        if (getDialog().getWindow() != null) getDialog().getWindow()
+                .setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    public void show(FragmentManager fragmentManager) {
         super.show(fragmentManager, TAG);
     }
 
